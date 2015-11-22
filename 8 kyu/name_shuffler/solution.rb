acad1da6219d1bb44457eca56041f0f7311f8faf
Nayed SAID ALI <nayed.saidali@gmail.com>
2015-11-22 23:23:09 +0100

    name shuffler

---
+++ 8 kyu/name_shuffler/solution.rb
@@ -0,0 +1,5 @@
+def name_shuffler(str)
+  str = str.split(" ")
+  str.reverse!
+  str = str.join(" ")
+end
---
+++ 8 kyu/name_shuffler/test.rb
@@ -0,0 +1,3 @@
+Test.assert_equals(name_shuffler('john McClane'),'McClane john')
+Test.assert_equals(name_shuffler('Mary jeggins'),'jeggins Mary')
+Test.assert_equals(name_shuffler('tom jerry'),'jerry tom')
