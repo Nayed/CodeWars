16a7d75b8e08b3320ebd3923f8de98c6670ef7f0
Nayed SAID ALI <nayed.saidali@gmail.com>
2015-11-30 23:52:04 +0100

    player to qualiifying stage

---
+++ 8 kyu/player_to_qualifuing_stage/test.rb
@@ -0,0 +1,2 @@
+Test.assert_equals(playerRankUp(64), false)
+Test.assert_equals(playerRankUp(100), "Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up.")
