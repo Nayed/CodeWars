6021b35f6c249611fb99416a6eca4ad87826cae5
Nayed SAID ALI <nayed.saidali@gmail.com>
2015-12-22 23:00:01 +0100

    count by x

---
+++ 8 kyu/count_by_x/test.rb
@@ -0,0 +1,5 @@
+Test.assert_equals(count_by(1, 5), [1, 2, 3, 4, 5])
+Test.assert_equals(count_by(2, 5), [2, 4, 6, 8, 10])
+Test.assert_equals(count_by(3, 5), [3, 6, 9, 12, 15])
+Test.assert_equals(count_by(50, 5), [50, 100, 150, 200, 250])
+Test.assert_equals(count_by(100, 5), [100, 200, 300, 400, 500])
