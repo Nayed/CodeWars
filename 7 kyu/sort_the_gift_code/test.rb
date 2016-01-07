322ebbb94b69cafa04d862c3f83e07d7b1a7db5f
Nayed SAID ALI <nayed.saidali@gmail.com>
2016-01-07 23:40:58 +0100

    sort the gift

---
+++ 7 kyu/sort_the_gift_code/test.rb
@@ -0,0 +1,3 @@
+Test.assert_equals(sort_gift_code('abcdef'), 'abcdef');
+Test.assert_equals(sort_gift_code('pqksuvy'), 'kpqsuvy');
+Test.assert_equals(sort_gift_code('zyxwvutsrqponmlkjihgfedcba'), 'abcdefghijklmnopqrstuvwxyz');
